2cafcc2a0c9af7048bc9f49889e8c20e427a0cd4
elad-rubinstein <60442241+elad-rubinstein@users.noreply.github.com>
2023-03-02 11:45:42 +0200

    constants and structs
    constants and structs

---
+++ Movie_Theater/CONSTANTS.h
@@ -0,0 +1,19 @@
+// MACROS
+
+#define TRUE 1
+#define FALSE 0
+
+#define NUM_OF_DAYS_IN_WEEK 7
+
+#define ONE 1
+#define TWO 2
+
+#define CHAIR_TAKEN 0
+#define CHAIR_UNOCCUPIED 1
+
+#define MAX(X,Y) X > Y ? X : Y
+#define MIN(X,Y) X < Y ? X : Y
+#define GET_MASK_LENGTH(seatsNum) (seatsNum - 1) / sizeof(char) + 1 
+
+#define NUM_OF_THEATERS 10
+#define SCREENING_HOURS_PER_DAY 8
--- Movie_Theater/Structs.h
+++ Movie_Theater/Structs.h
@@ -1,2 +1,46 @@
 // structs file
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <C:\Users\nadavru\Documents\GitHub\Movie_Theater\Movie_Theater\AVLHandler.h>
+#include <C:\Users\nadavru\Documents\GitHub\Movie_Theater\Movie_Theater\CONSTANTS.h>
+
+// String
+typedef char* String;
+
+typedef char Mask;
+typedef unsigned short us;
+
+// Movie
+typedef struct Moive {
+	String name;
+	float length;
+	Node* days[NUM_OF_DAYS_IN_WEEK];
+} *MoviePtr;
+
+// MovieTheater
+typedef struct MovieTheater {
+	us rowNum;
+	us ColNum;
+	us theaterId;
+} *MovieTheaterPtr;
+
+// Screenings
+typedef struct Screening {
+	us movieId;
+	us theaterId;
+	us hour;
+	Mask* seats;
+	us seatsLeft;
+} *ScreeningPtr;
+
+// Week and Day - schedule
+typedef struct DaySchedule {
+	ScreeningPtr screeningsSchedule[SCREENING_HOURS_PER_DAY][NUM_OF_THEATERS];
+} *DaySchedulePtr;
+
+typedef struct WeekSchedule {
+	DaySchedule weekSchedule[NUM_OF_DAYS_IN_WEEK];
+} *WeekSchedulePtr;
+
+
