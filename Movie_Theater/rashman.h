e8f135b194dd1543608e4eaf7bfbbaf9d5422d43
YuvalRashman <56125252+YuvalRashman@users.noreply.github.com>
2023-03-02 17:57:43 +0200

    fix

--- Movie_Theater/rashman.h
+++ Movie_Theater/rashman.h
@@ -3,57 +3,24 @@
 ScreeningPtr SearchScreeningByMovieId(int movieId);
 
 
-// what if the left is bigger
-Node* SearchClose(Node* root, int key) {
-	if (!root)
-	{
-		return root;
-	}
-	if (root->right)
-	{
-		if (root->right->key < key) // if the right key is smaller then yours go there
-		{
-			return SearchClose(root->right, smallest ,key);
-		}
-	}
-	if (root->left)
-	{
-		if (root->left->key > key) {
-			return SearchClose(root->left, key);
-		}
-	}
-	// need to check by the root from now to check if good
-	if (key > root->key) {
-		SearchClose(root->right, key);
-	}
-	return root;
+Node* ClosestSmallerKeyRec(Node* node, int key, Node *best) {
+    if (!node) { // if NULL
+        return best; // return the best so far
+    }
+    if (node->key == key) { // if equal we neeed to find smaller or best / father
+        if (node->left) {
+            return ClosestSmallerKey(node->left, key, node->left);
+        }
+        return best;
+    }
+    if (node->key < key) {
+        return ClosestSmallerKey(node->right, key, node);
+    }
+    return ClosestSmallerKey(node->left, key, best);
 }
 
-
-Node* closestKey(struct Node* node, int key) {
-    if (node)
-        return NULL;  // Return maximum integer value if the node is empty
-
-    int diff = node->key - key;
-
-    if (diff == 0)
-        return node;
-
-    if (diff > 0) {
-        Node* closestLeft = closestKey(node->left, key);
-
-        if (abs(node->key - key) < abs(closestLeft->key - key))
-            return node;
-        else
-            return closestLeft;
-    } else {
-        Node* closestRight = closestKey(node->right, key);
-
-        if (abs(node->key - key) < abs(closestRight->key - key))
-            return node;
-        else
-            return closestRight;
-    }
+Node* ClosestSmallerKey(Node* node, int key) {
+    return ClosestSmallerKeyRec(node, key, NULL);
 }
 
 
